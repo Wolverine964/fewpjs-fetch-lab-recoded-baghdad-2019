3102658ea2268b65ce35a7a0696f9863267344fd
ibraheem Hasan <rules656@gmail.com>
2019-10-14 15:08:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 function fetchBooks() {
   return fetch("https://anapioficeandfire.com/api/books")
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(json) {
-      console.log(json);
-      renderBooks(json);
-    });
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
 }
-​
+
 function renderBooks(json) {
-  const main = document.querySelector("main");
+  const main = document.querySelector('main')
   json.forEach(book => {
-    const h2 = document.createElement("h2");
-    h2.innerHTML = `<h2>${book.name}</h2>`;
-    main.appendChild(h2);
-  });
+    const h2 = document.createElement('h2')
+    h2.innerHTML = `<h2>${book.name}</h2>`
+    main.appendChild(h2)
+  })
 }
-​
-document.addEventListener("DOMContentLoaded", function() {
-  fetchBooks();
-});
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks()
+})
